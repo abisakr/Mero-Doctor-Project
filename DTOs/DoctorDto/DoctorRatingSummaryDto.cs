760c5fc72aeaecac6404dbf0c03e0160e04ab51e
abiskar <abiskar.ag@gmail.com>
2025-06-05 22:41:49 +0545

    Doctor Rating Completed

---
+++ DTOs/DoctorDto/DoctorRatingSummaryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mero_Doctor_Project.DTOs.DoctorDto
+{
+    public class DoctorRatingSummaryDto
+    {
+        public int AverageRating { get; set; } 
+        public List<RatingReviewGetDto> Reviews { get; set; }
+    }
+}
---
+++ DTOs/DoctorDto/RatingReviewCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mero_Doctor_Project.DTOs.DoctorDto
+{
+    public class RatingReviewCreateDto
+    {
+        public int DoctorId { get; set; }
+        public int Rating { get; set; }
+        public string? Review { get; set; }
+    }
+}
