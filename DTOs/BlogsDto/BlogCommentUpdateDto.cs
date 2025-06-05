5d38f6ddf4e5431777010278142fb32fc4a2a942
abiskar <abiskar.ag@gmail.com>
2025-06-05 21:36:04 +0545

    blog comment completed

---
+++ DTOs/BlogsDto/BlogCommentUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mero_Doctor_Project.DTOs.BlogsDto
+{
+    public class BlogCommentUpdateDto
+    {
+        [Required]
+        public int BlogCommentId { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Comment { get; set; }
+    }
+}
