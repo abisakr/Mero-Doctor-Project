aaa66f448c364f1f275390705b0dd854fd21859b
abiskar <abiskar.ag@gmail.com>
2025-06-05 21:52:46 +0545

    feedback completed

--- Repositories/Interfaces/IFeedbackRepository.cs
+++ Repositories/Interfaces/IFeedbackRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Mero_Doctor_Project.Repositories.Interfaces
+﻿using Mero_Doctor_Project.DTOs.FeedbackDto;
+using Mero_Doctor_Project.Models.Common;
+
+namespace Mero_Doctor_Project.Repositories.Interfaces
 {
     public interface IFeedbackRepository
     {
+        Task<ResponseModel<string>> CreateFeedbackAsync(FeedbackCreateDto dto);
+        Task<ResponseModel<List<FeedbackGetDto>>> GetAllFeedbacksAsync();
     }
 }
