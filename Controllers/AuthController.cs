aa73fadb7dcdfebc13ae32e923a4b351188c6fa5
abiskar <abiskar.ag@gmail.com>
2026-07-23 16:15:23 +0545

    chenges small

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -71,8 +71,8 @@ namespace Mero_Doctor_Project.Controllers
             //var resetLink = $"merodoctor://reset-password?token={Uri.EscapeDataString(token)}&email={Uri.EscapeDataString(user.Email)}";
 
             // Send Email
-            string fromMail = "akash.ag@gmail.com";
-            string fromPassword = "ejsssfadoe";
+            string fromMail = "abiskar.ag@gmail.com";
+            string fromPassword = "rsfotrjpmphyhlqn";
 
             MailMessage message = new MailMessage();
             message.From = new MailAddress(fromMail);
